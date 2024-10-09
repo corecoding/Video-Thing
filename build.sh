7d25610a08b0e5a4c02bf09b1e1590100e59aa73
Chris Monahan <chris@corecoding.com>
2024-10-08 22:55:51 -0400

    build script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+rm -rf Video Thing.app/
+
+pip3 install PyQt6
+
+if [ ! -f ./ffmpeg ]; then
+  #curl -JL -o ffmpeg.zip https://evermeet.cx/ffmpeg/get/zip
+  curl -JL -o ffmpeg.zip https://evermeet.cx/ffmpeg/getrelease/zip
+  unzip ffmpeg.zip
+  rm ffmpeg.zip
+fi
+
+if [ ! -f ./ffprobe ]; then
+  #curl -JL -o ffprobe.zip https://evermeet.cx/ffprobe/get/zip
+  curl -JL -o ffprobe.zip https://evermeet.cx/ffmpeg/getrelease/ffprobe/zip
+  unzip ffprobe.zip
+  rm ffprobe.zip
+fi
+
+PATH='/Users/cmonahan/Library/Application Support/Herd/bin/:/Users/cmonahan/Library/Application Support/Herd/config/nvm/versions/node/v21.7.3/bin:~/Library/Python/3.9/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin'
+
+pyinstaller --hidden-import PyQt6 --hidden-import PyQt6.QtCore --hidden-import PyQt6.QtGui --hidden-import PyQt6.QtWidgets --clean --windowed --name "Video Thing" --add-data="./ffmpeg:." --add-data="./ffprobe:." ./app.py
+
+if [ -f ./ffmpeg ]; then
+  rm ffmpeg
+fi
+
+if [ -f ./ffprobe ]; then
+  rm ffprobe
+fi
+
+mv dist/Video\ Thing.app .
+
+rm -rf build
+rm -rf dist
