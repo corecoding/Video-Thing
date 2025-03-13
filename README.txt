321850c0651b821b7c3df70e9cbed318eab97365
Chris Monahan <chris@corecoding.com>
2025-03-12 23:40:43 -0400

    remove readme from build

---
+++ README.txt
@@ -0,0 +1,13 @@
+Video Thing
+
+This application requires Python 3 to be installed on your Mac.
+If you don't have Python 3 installed, please download it from:
+https://www.python.org/downloads/
+
+When you first launch the app, it will check for and install
+any required Python packages using pip.
+
+Requirements:
+- Python 3.6 or higher
+- pip (which comes with Python)
+- Python's venv module (should come with Python)
--- build.sh
+++ build.sh
@@ -153,23 +153,6 @@ cat > "$CONTENTS_DIR/Info.plist" << EOF
 </plist>
 EOF
 
-# Create README file
-cat > "$RESOURCES_DIR/README.txt" << EOF
-Video Thing
-
-This application requires Python 3 to be installed on your Mac.
-If you don't have Python 3 installed, please download it from:
-https://www.python.org/downloads/
-
-When you first launch the app, it will check for and install
-any required Python packages using pip.
-
-Requirements:
-- Python 3.6 or higher
-- pip (which comes with Python)
-- Python's venv module (should come with Python)
-EOF
-
 # Clean up temp files
 echo "Cleaning up temporary files..."
 rm -rf "$TEMP_DIR"
